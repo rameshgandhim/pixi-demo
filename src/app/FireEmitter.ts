474786c141f54516dbeb8e55b45c9d8488cf2550
Ramesh Gandhi Mathiyalagan <rameshgandhim@gmail.com>
2021-10-16 22:18:24 +0530

    add spark

--- src/app/FireEmitter.ts
+++ src/app/FireEmitter.ts
@@ -2,7 +2,7 @@
 /* eslint-disable import/prefer-default-export */
 export const fireEmitter: any = {
   'alpha': {
-    'start': 0.62,
+    'start': 0.8,
     'end': 0,
   },
   'scale': {
@@ -15,12 +15,12 @@ export const fireEmitter: any = {
     'end': '#ff622c',
   },
   'speed': {
-    'start': 500,
+    'start': 50,
     'end': 500,
     'minimumSpeedMultiplier': 1,
   },
   'acceleration': {
-    'x': 0,
+    'x': 100,
     'y': 0,
   },
   'maxSpeed': 0,
@@ -35,7 +35,7 @@ export const fireEmitter: any = {
   },
   'lifetime': {
     'min': 0.1,
-    'max': 0.75,
+    'max': 0.95,
   },
   'blendMode': 'normal',
   'frequency': 0.001,
@@ -50,7 +50,7 @@ export const fireEmitter: any = {
   'spawnCircle': {
     'x': 0,
     'y': 0,
-    'r': 10,
+    'r': 1000,
   },
   'behaviors': [
     {
@@ -60,7 +60,7 @@ export const fireEmitter: any = {
           'list': [
             {
               'time': 0,
-              'value': 0.62,
+              'value': 1,
             },
             {
               'time': 1,
@@ -73,7 +73,7 @@ export const fireEmitter: any = {
     {
       'type': 'moveSpeedStatic',
       'config': {
-        'min': 500,
+        'min': 50,
         'max': 500,
       },
     },
@@ -84,15 +84,15 @@ export const fireEmitter: any = {
           'list': [
             {
               'time': 0,
-              'value': 0.25,
+              'value': 0.6,
             },
             {
               'time': 1,
-              'value': 0.75,
+              'value': 0.2,
             },
           ],
         },
-        'minMult': 1,
+        'minMult': 1.1,
       },
     },
     {
@@ -117,17 +117,17 @@ export const fireEmitter: any = {
       'config': {
         'accel': 0,
         'minSpeed': 50,
-        'maxSpeed': 50,
+        'maxSpeed': 100,
         'minStart': 265,
         'maxStart': 275,
       },
     },
     {
-      'type': 'textureRandom',
+      'type': 'textureOrdered',
       'config': {
         'textures': [
-          '/assets/img/particle.png',
           '/assets/img/fire.png',
+          '/assets/img/particle.png',
         ],
       },
     },
@@ -138,8 +138,158 @@ export const fireEmitter: any = {
         'data': {
           'x': 0,
           'y': 0,
-          'radius': 10,
-          'innerRadius': 0,
+          'radius': 75,
+          'innerRadius': 10,
+          'affectRotation': false,
+        },
+      },
+    },
+  ],
+};
+
+export const sparkEmitter: any = {
+  'alpha': {
+    'start': 0,
+    'end': 1,
+  },
+  'scale': {
+    'start': 0.25,
+    'end': 1,
+    'minimumScaleMultiplier': 1,
+  },
+  'color': {
+    'start': '#fff191',
+    'end': '#ff622c',
+  },
+  'speed': {
+    'start': 50,
+    'end': 500,
+    'minimumSpeedMultiplier': 1,
+  },
+  'acceleration': {
+    'x': 100,
+    'y': 0,
+  },
+  'maxSpeed': 0,
+  'startRotation': {
+    'min': 265,
+    'max': 275,
+  },
+  'noRotation': false,
+  'rotationSpeed': {
+    'min': 50,
+    'max': 50,
+  },
+  'lifetime': {
+    'min': 0.1,
+    'max': 0.95,
+  },
+  'blendMode': 'normal',
+  'frequency': 0.001,
+  'emitterLifetime': -1,
+  'maxParticles': 1000,
+  'pos': {
+    'x': 0,
+    'y': 0,
+  },
+  'addAtBack': false,
+  'spawnType': 'circle',
+  'spawnCircle': {
+    'x': 0,
+    'y': 0,
+    'r': 1000,
+  },
+  'behaviors': [
+    {
+      'type': 'alpha',
+      'config': {
+        'alpha': {
+          'list': [
+            {
+              'time': 0,
+              'value': 0,
+            },
+            {
+              'time': 0.4,
+              'value': 0,
+            },
+            {
+              'time': 1,
+              'value': 1,
+            },
+          ],
+        },
+      },
+    },
+    {
+      'type': 'moveSpeedStatic',
+      'config': {
+        'min': 50,
+        'max': 500,
+      },
+    },
+    {
+      'type': 'scale',
+      'config': {
+        'scale': {
+          'list': [
+            {
+              'time': 0,
+              'value': 0.6,
+            },
+            {
+              'time': 1,
+              'value': 0.9,
+            },
+          ],
+        },
+        'minMult': 1.1,
+      },
+    },
+    {
+      'type': 'color',
+      'config': {
+        'color': {
+          'list': [
+            {
+              'time': 0,
+              'value': 'fff191',
+            },
+            {
+              'time': 1,
+              'value': 'ff622c',
+            },
+          ],
+        },
+      },
+    },
+    {
+      'type': 'rotation',
+      'config': {
+        'accel': 0,
+        'minSpeed': 50,
+        'maxSpeed': 100,
+        'minStart': 265,
+        'maxStart': 275,
+      },
+    },
+    {
+      'type': 'textureOrdered',
+      'config': {
+        'textures': [
+          '/assets/img/spark.png',
+        ],
+      },
+    },
+    {
+      'type': 'spawnShape',
+      'config': {
+        'type': 'torus',
+        'data': {
+          'x': 0,
+          'y': 0,
+          'radius': 75,
+          'innerRadius': 10,
           'affectRotation': false,
         },
       },
--- src/app/FireParticle.ts
+++ src/app/FireParticle.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { Emitter } from '@pixi/particle-emitter';
-import { fireEmitter } from './FireEmitter';
+import { fireEmitter, sparkEmitter } from './FireEmitter';
 import { GameObject } from './GameObject';
 import { ITick } from './ITick';
 import { ScreenConfig } from './ScreenConfig';
@@ -8,21 +8,26 @@ import { ScreenConfig } from './ScreenConfig';
 export class FireParticle extends GameObject implements ITick {
   emitter: Emitter;
 
+  emitter2: Emitter;
+
   constructor(screen: ScreenConfig) {
     super();
     this.x = screen.width / 2;
     this.y = screen.height - 200;
     this.emitter = new Emitter(this, { ...fireEmitter, autoUpdate: true });
+    this.emitter2 = new Emitter(this, { ...sparkEmitter, autoUpdate: true });
   }
 
   activate(): void {
     // this.addChild(this.particleContainer);
     this.emitter.emit = true;
+    this.emitter2.emit = true;
     super.activate();
   }
 
   deactivate(): void {
     this.emitter.emit = false;
+    this.emitter2.emit = false;
     // this.removeChildren();
     super.deactivate();
   }
